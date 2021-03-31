5afd2301ca797d2b58ba2788519f02066f95e64a
DavDan1 <david-danielyan@hotmail.com>
2021-03-31 21:03:31 +0200

    the test is giving error, added insufficient funds to the code

--- lib/atm.rb
+++ lib/atm.rb
@@ -6,9 +6,32 @@ class Atm
         @funds=1000
     end
 
-    def withdraw(amount)
-        @funds -= amount
+    def withdraw(amount,account)
+
+       case
+       when insufficient_funds_in_account?(amount, account)
+        {status:false, message:'insufficient funds', date: Time:now}
+       else
+        perform_transaction(amount, account)
         
     end
-
 end
+
+
+
+private
+#changed the methods
+
+def insufficient_funds_in_account?(amount, account)
+    amount>account.balance
+    
+end
+
+def perform_transaction(amount, account)
+    @fund -= amount
+    account.balance=account.balance-amount
+    {status:true, message:'success', date: Time.now , amount:amount}
+    
+end
+end
+
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/atm'
+require_relative '../lib/atm.rb'
 
 describe Atm do
     let(:account){instance_double('Acount')}
@@ -20,5 +20,15 @@ describe Atm do
         subject.withdraw(150)
         expect(subject.funds).to eq 850
     end
+    it 'allow withdrawl when there is enough money.' do
+     expected_output={status:true, message:'success', date: Time.now , amount:45}
+     expect(subject.withdrawl(45, account)).to eq expected_output   
+    
+    end
+    it 'rejects when there is not enough cash' do
+        expected_output={status:false, message:'insufficient funds', date: Time:now}
+        expect(subject.withdraw(105,account)).to eq expected_output
+    end
 
 end
+
