487384272c52c5695fa9c64164652e2e6c7f8341
DavDan1 <david-danielyan@hotmail.com>
2021-04-01 19:53:35 +0200

    solved the error issue, added insufficient funds and defined it

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,37 +1,41 @@
-# we completed the atm rb file
 class Atm
     attr_accessor :funds
 
     def initialize
-        @funds=1000
+        @funds = 1000
     end
 
-    def withdraw(amount,account)
-
-       case
-       when insufficient_funds_in_account?(amount, account)
-        {status:false, message:'insufficient funds', date: Time:now}
-       else
-        perform_transaction(amount, account)
+    def withdraw(amount, account)
         
+        case 
+        when insuifficient_funds_in_account?(amount, account)
+            {status: false, message: 'insufficient funds'}
+            
+        else
+            perform_transaction(amount, account)
+            
+        end
     end
+
+    private
+
+    def insuifficient_funds_in_account?(amount, account)
+        amount > account.balance
+    end
+
+    def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true , message: 'success', amount: amount}
+    end
+
+
+
+
 end
-
-
-
-private
-#changed the methods
-
-def insufficient_funds_in_account?(amount, account)
-    amount>account.balance
     
-end
 
-def perform_transaction(amount, account)
-    @fund -= amount
-    account.balance=account.balance-amount
-    {status:true, message:'success', date: Time.now , amount:amount}
-    
-end
-end
-
+  
+     
+  
+  
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,34 +1,35 @@
 require_relative '../lib/atm.rb'
 
 describe Atm do
-    let(:account){instance_double('Acount')}
-
+    let(:account){instance_double('Account')}
     before do
         allow(account).to receive(:balance).and_return(100)
         allow(account).to receive(:balance=)
     end
-    
-    it 'is expected to be an instance of Atm' do
-        expect(subject.class).to eq Atm
-    end
-
-    it 'is expected to hold 1000 currency an initialize' do
+    it 'has 1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
     
-    it 'is expected reduce funds on successful withdrawal' do
-        subject.withdraw(150)
-        expect(subject.funds).to eq 850
+    it 'funds are reduced at withdraw' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
+
     end
-    it 'allow withdrawl when there is enough money.' do
-     expected_output={status:true, message:'success', date: Time.now , amount:45}
-     expect(subject.withdrawl(45, account)).to eq expected_output   
+    it 'allow withdraw if the account has enough balance.' do
+        expected_output = { status:true, message: 'success', amount: 45} #time.now works! but date doeesn't :(
+        expect(subject.withdraw(45, account)).to eq expected_output
+        
+    end
+    it 'reject withdraw if account has insufficient funds' do
+        expected_output= {status: false, message:  'insufficient funds'}
+        expect(subject.withdraw(105, account)).to eq expected_output
+    end
     
-    end
-    it 'rejects when there is not enough cash' do
-        expected_output={status:false, message:'insufficient funds', date: Time:now}
-        expect(subject.withdraw(105,account)).to eq expected_output
-    end
 
 end
+
+
+
+
+
 
